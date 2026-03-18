21fa23c83c7ec1ff5b66e9dfb7b8e78972f49619
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-18 16:59:40 -0300

    Update Password Schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,10 +1,18 @@
 import z from "zod";
 
+const passwordSchema = z.string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(100, 'Password must be at most 100 characters')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one number')
+    .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
+
 export const registerSchema = z.object({
     body: z.object({
         username: z.string().min(3).max(50),
         email: z.email(),
-        password: z.string().min(6).max(100)
+        password: passwordSchema
     })
 });
 
