30d748f0ab84410d95d42b10e36f2cba7f261199
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-24 15:24:41 -0300

    Feat Helmet and Cors

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,33 @@ dotenv.config();
 import express from 'express';
 import router from './routes/routes';
 import { errorHandler } from './middlewares/error.middleware';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: { success: false, message: 'Too many requests, please try again later' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message: { success: false, message: 'Too many attempts, please try again later' },
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+app.use('/api', limiter, router);
+app.use('/api/auth', authLimiter);
 app.use(errorHandler);
 
 export default app;
